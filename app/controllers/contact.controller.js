2f79e4a5faa657a4f87148b920776c09613ba5c0
Your Name <phuong0987654321@gmail.com>
2022-09-01 14:33:07 +0700

    Cai dat xu ly loi

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,21 +1,21 @@
 exports.create = (req, res) => {
-    res.send({mesage: "create handler"})
+    res.send({message: "create handler"})
 };
 exports.findAll = (req, res) => {
-    res.send({mesage: "findAll handler"})
+    res.send({message: "findAll handler"})
 };
 exports.findOne = (req, res) => {
-    res.send({mesage: "findOne handler"})
+    res.send({message: "findOne handler"})
 };
 exports.update = (req, res) => {
-    res.send({mesage: "update handler"})
+    res.send({message: "update handler"})
 };
 exports.delete = (req, res) => {
-    res.send({mesage: "delete handler"})
+    res.send({message: "delete handler"})
 };
 exports.deleteAll = (req, res) => {
-    res.send({mesage: "deleteAll handler"})
+    res.send({message: "deleteAll handler"})
 };
 exports.findAllFavorite = (req, res) => {
-    res.send({mesage: "findAllFavorite handler"})
+    res.send({message: "findAllFavorite handler"})
 };
